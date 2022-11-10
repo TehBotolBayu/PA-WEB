f34ad2409b8844fc3b2ecc1dbc38855495d77f74
Venomz22 <93197543+Venomz22@users.noreply.github.com>
2022-11-10 22:22:35 +0800

    Add files via upload

---
+++ formapply.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="formhire.css">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="title">
+          BUAT LAYANAN
+        </div>
+        <div class="form">
+           <div class="inputfield">
+              <label><b>Judul</b></label>
+              <input type="text" class="input">
+           </div>  
+          <div class="inputfield">
+              <label><b>Skill</b></label>
+              <input type="text" class="input">
+           </div> 
+            <div class="inputfield">
+              <label><b>Harga</b></label>
+              <input type="number" class="input">
+           </div> 
+          <div class="inputfield">
+              <label><b>Tanggal</b></label>
+              <input type="date" class="input">
+           </div> 
+          <div class="inputfield">
+              <label><b>Deskripsi</b></label>
+              <textarea class="textarea"></textarea>
+           </div> 
+          <div class="inputfield">
+              <label><b>Gambar</b></label>
+              <input type="file" class="input">
+           </div> 
+          <div class="inputfield">
+            <input type="submit" value="KIRIM" class="btn">
+          </div>
+        </div>
+    </div>
+</body>
+</html>
